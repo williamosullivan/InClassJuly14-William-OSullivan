5aa7bdfd56854960ea12cbd9e1897968910decdc
William O'Sullivan <williamosullivan@gmail.com>
2014-07-14 15:33:14 -0400

    Initial Create
    Created

---
+++ InClassJuly14-William-OSullivan/InClassJuly14-William-OSullivan/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassJuly14_William_OSullivan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
