516455ff22d231170c74513f106ca75075f60e57
William O'Sullivan <williamosullivan@gmail.com>
2014-07-14 16:19:17 -0400

    Created Code
    Tested and working

---
+++ InClassJuly14-William-OSullivan/InClassJuly14-William-OSullivan/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InClassJuly14_William_OSullivan
+{
+    class Person
+    {
+        int age { get; set; }
+        string name { get; set; }
+        DateTime birthdate { get; set; }
+    }
+}
