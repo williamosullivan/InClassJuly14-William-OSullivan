e2a7ef9fc12d44e5598675e6b14d628146ec967b
William O'Sullivan <williamosullivan@gmail.com>
2014-07-14 16:19:24 -0400

    Final
    Final commit

--- InClassJuly14-William-OSullivan/InClassJuly14-William-OSullivan/Program.cs
+++ InClassJuly14-William-OSullivan/InClassJuly14-William-OSullivan/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace InClassJuly14_William_OSullivan
     {
         static void Main(string[] args)
         {
+            List<Person> people = new List<Person> { };
+            string[] array = new string[] { "1", "2", "3", "4" };
+            Dictionary<string, Person> map = new Dictionary<string, Person>();
+
+            foreach (string i in array)
+            {
+                map[i] = new Person();
+                people.Add(map[i]);
+                Console.WriteLine(map[i]);
+                Console.ReadLine();
+            }
         }
     }
 }
